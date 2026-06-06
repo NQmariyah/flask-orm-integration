47de4a7bd60bba9fcad7a5866c913c2e8d7af39d
nqomariyah <nqomariyah@polinela.ac.id>
2026-06-06 18:44:08 +0700

    init jwt config, user models and its relations

--- todomodels.py
+++ todomodels.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -8,21 +10,46 @@ DATABASE_NAME = 'todoapp.db'
 DATABASE_PATH = os.path.join(app.instance_path, DATABASE_NAME)
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DATABASE_NAME}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = "iet solid solid sulit"
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(hours=1)
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 
 # berikut adalah definisi model yang digunakan dalam ORM
 class Todo(db.Model):
+    __tablename__ = 'todo'
+
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(100), nullable=False)
     completed = db.Column(db.Boolean, default=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def to_dict(self):
         return {
             "id": self.id,
             "item": self.item,
             "completed": bool(self.completed)
+            "user": self.user_id
+        }
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    todo = db.relationship('Todo', backref='user', lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "todo": self.todo
         }
 
 
